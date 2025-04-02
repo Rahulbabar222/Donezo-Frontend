d5039eee4135f440a546ecb50684aed72b43e9d8
Rahul Babar <rahulbabar222@gmail.com>
2025-04-02 17:56:03 +0530

    Pomodoro state Migration completed

--- src/components/Pomodoro/Pomodoro.jsx
+++ src/components/Pomodoro/Pomodoro.jsx
@@ -77,8 +77,11 @@ const Pomodoro = () => {
             </div>
 
             <div className="flex gap-8">
-                <button className="hover:scale-110" onClick={() => dispatch(reset())}>
-                    🔄
+                <button className="hover:scale-110" onClick={() =>  dispatch(reset())}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32" height="32" color="#dce0fe" fill="none">
+                        <path d="M5.04798 8.60657L2.53784 8.45376C4.33712 3.70477 9.503 0.999914 14.5396 2.34474C19.904 3.77711 23.0904 9.26107 21.6565 14.5935C20.2227 19.926 14.7116 23.0876 9.3472 21.6553C5.36419 20.5917 2.58192 17.2946 2 13.4844" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                        <path d="M12 8V12L14 14" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                    </svg>
                 </button>
                 <button className="text-[#dce0fe] text-4xl p-4 rounded-full bg-[#272b51] hover:border-1" onClick={() => dispatch(toggleRunning())}>
                     {isRunning ? "P A U S E" : "S T A R T"}
